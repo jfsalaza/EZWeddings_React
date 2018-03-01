b3c5c3a6719de3bb234faef3ff9f7718172e4193
izeferino94 <izeferin@ucsd.edu>
2018-03-01 11:16:16 -0800

    working on partner page, working on dynamic state

--- src/components/partner/Chat.js
+++ src/components/partner/Chat.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Message from './Message';
 import '../../styles/business.css';
 
-const Chat = (props) => {
-    return(
-        <div className="page-section">
-            <h3>Chat</h3>
-            <div id="chat-box">
+class Chat extends React.Component {
+    state = {
+        messages: [
+        ]
+    };
+
+    sendMessage = (e) => {
+            if(e.keyCode == 13) {
+                console.log("HELLO!");
+                this.setState((prevState) => 
+                {
+                        var next = prevState.messages.slice();
+                        next.push({sender:"Me: ", msg:"New"})                        
+                        return {messages: next}
+                });
+            }
+    }
+
+    render(){
+        return(
+            <div className="page-section">
+                <h3>Chat</h3>
+                <div id="chat-box">
+                    {
+                        this.state.messages.map((message) => {
+                            return <Message sender={message.sender} message={message.msg}></Message>
+                        })
+                    }
+                </div>
+                <input id="msg-input" type="text" name="msg" placeholder="Type message..." onKeyUp={this.sendMessage}/>
             </div>
-            <input id="msg-input" type="text" name="msg" placeholder="Type message..." onkeyup="sendMessage(event)"/>
-        </div>
-    );
-};
+        );
+    }    
+}
 
 export default Chat;
